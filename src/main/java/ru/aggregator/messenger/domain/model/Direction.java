e71d5715e24d21060575890d0a99e153a843fdfb
Передереев Илья Николаевич <peredereevin@gmail.com>
2026-04-28 17:36:20 +0300

    Create Direction.java

---
+++ src/main/java/ru/aggregator/messenger/domain/model/Direction.java
@@ -0,0 +1,9 @@
+package ru.aggregator.messenger.domain.model;
+
+/**
+ * Направление сообщения.
+ */
+public enum Direction {
+    IN,   // от пользователя к системе
+    OUT   // от системы к пользователю
+}
