dcccf78114da6a0343ad9473b23dd65576379dbd
Передереев Илья Николаевич <peredereevin@gmail.com>
2026-04-28 17:34:58 +0300

    Create ChannelType.java

---
+++ src/main/java/ru/aggregator/messenger/domain/model/ChannelType.java
@@ -0,0 +1,11 @@
+package ru.aggregator.messenger.domain.model;
+
+/**
+ * Типы поддерживаемых мессенджеров.
+ * Каждый новый канал добавляется сюда.
+ */
+public enum ChannelType {
+    VK,
+    TELEGRAM,
+    WHATSAPP
+}
